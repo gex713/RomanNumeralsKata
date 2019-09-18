129fb1e04cf97b0ab5e2cc4ffd41b9e9cd03624a
Houston.Miller <houston.miller@ministrybrands.com>
2019-09-18 16:21:56 -0400

    Refactored to get rid of some more duplication as a pattern emerged with the Vs. Takes advantage of the for loop that we had that appends Is.

--- RomanNumerals/RomanNumerals.cs
+++ RomanNumerals/RomanNumerals.cs
@@ -12,25 +12,22 @@ namespace RomanNumerals
         public string ArabicToRoman(int number)
         {
             string numerals = String.Empty;
+            int remainder = number;
 
-            if(number == 6)
-            {
-                numerals += "VI";
-            }
-            else if (number == 5)
+            if (number >= 5)
             {
                 numerals += "V";
+                remainder -= 5;
             }
             else if (number == 4)
             {
                 numerals += "IV";
+                remainder -= 4;
             }
-            else
+
+            for (int i = 0; i < remainder; i++)
             {
-                for (int i = 0; i < number; i++)
-                {
-                    numerals += "I";
-                }
+                numerals += "I";
             }
 
             return numerals;
